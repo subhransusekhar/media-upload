4d4703c3ef91b1bbb3b9cfdf4e33aeb8f0d3333f
Subhransu Sekhar <subhransu.wsd@nic.in>
2016-06-10 11:51:21 +0530

    Initial Module Code Upload

---
+++ media-upload.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ * Plugin Name: Upload Multiple Files
+ * Plugin URI: http://pietaslabs.com
+ * Description: Module to Upload Multiple Media Files
+ * Version:
+ * Author: Subhransu Sekhar
+ * Author URI: pietaslabs.com
+ * License: GPLv2 or later
+ * TextDomain: files-meta
+ * DomainPath:
+ * Network: true
+ */
+
+ $meta_box_media_upload = new Media_Upload();
+
+ class Media_Upload {
+
+ 	function __construct() {
+ 		add_action( 'add_meta_boxes', array( $this, 'setup_media_box' ) );
+ 		add_action( 'save_post', array( $this, 'save_media_box' ), 10, 2 );
+ 	}
+
+ 	function setup_media_box() {
+ 		add_meta_box( 'meta_box_id', __( 'Media Upload', 'upload-meta-box' ), array( $this, 'media_meta_box_contents' ), 'post', 'normal' );
+ 	}
+
+ 	function media_meta_box_contents() {
+
+ 		wp_enqueue_media();
+ 		wp_enqueue_script( 'upload-meta-box-media', plugins_url('media.js', __FILE__ ), array('jquery') );
+
+ 		wp_nonce_field( 'nonce_action', 'nonce_name' );
+    $media_fieldcount = get_post_meta( get_the_id(), 'media_fieldcount', true );
+ 		// one or more
+ 		$field_names = array( 'media1');
+    if(!isset($media_fieldcount) || empty($media_fieldcount)) {
+      $media_fieldcount = count($field_names);
+    }
+    else {
+      for($i=2; $i < $media_fieldcount+1; $i++) {
+        $field_names[] = 'media'.$i;
+      }
+    }
+    echo "<input type='hidden' id='media_fieldcount'  class='small-text' name='media_fieldcount' value='$media_fieldcount' />";
+ 		foreach ( $field_names as $name ) {
+
+ 			$value = $rawvalue = get_post_meta( get_the_id(), $name, true );
+
+ 			$name = esc_attr( $name );
+ 			$value = esc_attr( $rawvalue );
+
+      echo "<input type='hidden' id='$name-value'  class='small-text'       name='meta-box-media[$name]' value='$value' />";
+ 			echo "<input type='button' id='$name'        class='button meta-box-upload-button'        value='Upload' />";
+ 			echo "<input type='button' id='$name-remove' class='button meta-box-upload-button-remove' value='Remove' />";
+      if($name == 'media'.$media_fieldcount) {
+        echo "<input type='button' id='$name-addmore' class='button meta-box-upload-button-addmore' value='Add More' />";
+      }
+      $attachment_meta = '';
+      if($rawvalue) {
+        $attachment_meta = wp_get_attachment_link( $rawvalue );
+      }
+
+ 			echo "<div class='image-preview'>$attachment_meta</div>";
+
+ 			echo '<br />';
+
+ 		}
+
+ 	}
+
+ 	function save_media_box( $post_id, $post ) {
+ 		if ( ! isset( $_POST['nonce_name'] ) ) //make sure our custom value is being sent
+ 			return;
+ 		if ( ! wp_verify_nonce( $_POST['nonce_name'], 'nonce_action' ) ) //verify intent
+ 			return;
+ 		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) //no auto saving
+ 			return;
+ 		if ( ! current_user_can( 'edit_post', $post_id ) ) //verify permissions
+ 			return;
+    update_post_meta( $post_id, 'media_fieldcount', $_POST['media_fieldcount'] ); //Save Field Count
+ 		$new_value = array_map( 'intval', $_POST['meta-box-media'] ); //sanitize
+ 		foreach ( $new_value as $k => $v ) {
+ 			update_post_meta( $post_id, $k, $v ); //save
+ 		}
+
+ 	}
+
+ }
---
+++ media.js
@@ -0,0 +1,64 @@
+jQuery( function ( $ ) {
+	var file_frame = [],
+		$button = $( '.meta-box-upload-button' ),
+		$removebutton = $( '.meta-box-upload-button-remove' );
+    $addmorebutton = $( '.meta-box-upload-button-addmore' );
+	$('#meta_box_id').on( 'click', '.meta-box-upload-button', function ( event ) {
+    console.log(this);
+		event.preventDefault();
+		var $this = $( this ),
+			id = $this.attr( 'id' );
+		// If the media frame already exists, reopen it.
+		if ( file_frame[ id ] ) {
+			file_frame[ id ].open();
+
+			return;
+		}
+		// Create the media frame.
+		file_frame[ id ] = wp.media.frames.file_frame = wp.media( {
+			title    : $this.data( 'uploader_title' ),
+			button   : {
+				text : $this.data( 'uploader_button_text' )
+			},
+			multiple : false  // Set to true to allow multiple files to be selected
+		} );
+
+		// When an image is selected, run a callback.
+		file_frame[ id ].on( 'select', function() {
+
+			// We set multiple to false so only get one image from the uploader
+			var attachment = file_frame[ id ].state().get( 'selection' ).first().toJSON();
+
+			// set input
+			$( '#' + id + '-value' ).val( attachment.id );
+
+			// set preview
+			var img = attachment.filename + ' (' + attachment.filesizeHumanReadable + ')';
+
+			$this.next( 'input' ).next( 'input' ).next( '.image-preview' ).html( img );
+
+		} );
+
+		// Finally, open the modal
+		file_frame[ id ].open();
+
+	} );
+
+	$('#meta_box_id').on( 'click', '.meta-box-upload-button-remove', function( event ) {
+		event.preventDefault();
+		var $this = $( this ),
+			id = $this.prev( 'input' ).attr( 'id' );
+		$this.next().next( '.image-preview' ).html( '' );
+		$( '#' + id + '-value' ).val( 0 );
+
+	} );
+  $('#meta_box_id').on( 'click', '.meta-box-upload-button-addmore', function( event ) {
+		event.preventDefault();
+		var $this = $( this );
+    var mediacount = parseInt($( '#media_fieldcount' ).val()) + 1;
+    $( '#media_fieldcount' ).val(mediacount);
+    var $name = 'media'+mediacount;
+		$this.next( 'div' ).next( 'br' ).after("<input type='hidden' id='"+ $name +"-value'  class='small-text' name='meta-box-media["+ $name +"]' value='' /><input type='button' id='"+ $name +"' class='button meta-box-upload-button' value='Upload' /><input type='button' id='"+ $name +"-remove' class='button meta-box-upload-button-remove' value='Remove' /><input type='button' id='"+ $name +"-addmore' class='button meta-box-upload-button-addmore' value='Add More' /><div class='image-preview'></div><br/>");
+    $this.hide();
+	} );
+} );
